88e7efecc6f03526755dacbf896a5e367a9f01fd
qyzhao <qyzhao@thoughtworks.com>
2020-01-10 14:28:23 +0800

    [Qiaoying] refactor: extract method about if input number is times of some data

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,13 +2,29 @@ package com.thoughtworks;
 
 class FizzBuzz {
     public String convertNumber(int inputNumber) {
-        if (inputNumber % 3 == 0) {
+        if (numberIsTimesOf3(inputNumber)) {
             return "Fizz";
-        } else if (inputNumber % 5 == 0) {
+        } else if (numberIsTimesOf5(inputNumber)) {
             return "Buzz";
-        } else if (inputNumber % 7 == 0) {
+        } else if (numberIsTimesOf7(inputNumber)) {
             return "Whizz";
         }
         return String.valueOf(inputNumber);
+    }
+
+    private boolean numberIsTimesOf7(int inputNumber) {
+        return inputNumberIsTimesOfGivenNumber(inputNumber, 7);
+    }
+
+    private boolean numberIsTimesOf5(int inputNumber) {
+        return inputNumberIsTimesOfGivenNumber(inputNumber, 5);
+    }
+
+    private boolean numberIsTimesOf3(int inputNumber) {
+        return inputNumberIsTimesOfGivenNumber(inputNumber, 3);
+    }
+
+    private boolean inputNumberIsTimesOfGivenNumber(int inputNumber, int givenNumber) {
+        return inputNumber % givenNumber == 0;
     }
 }
