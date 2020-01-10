958304ff71fd79df97257f6afe7aa652c4cd99ca
qyzhao <qyzhao@thoughtworks.com>
2020-01-10 14:23:22 +0800

    [Qiaoying] implement when given a number is times of 5, should return buzz

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,6 +2,11 @@ package com.thoughtworks;
 
 class FizzBuzz {
     public String convertNumber(int inputNumber) {
-        return "Fizz";
+        if (inputNumber % 3 == 0) {
+            return "Fizz";
+        } else if (inputNumber % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(inputNumber);
     }
 }
