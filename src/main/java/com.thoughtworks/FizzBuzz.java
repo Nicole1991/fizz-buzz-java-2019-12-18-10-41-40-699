aef5f7a976d04ff11adccfc56e4f81810d31358b
qyzhao <qyzhao@thoughtworks.com>
2020-01-10 15:01:36 +0800

    [Qiaoying] implement when given a number is contains 3, should return value fizz

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -3,17 +3,19 @@ package com.thoughtworks;
 class FizzBuzz {
     public String convertNumber(int inputNumber) {
         String result = "";
-        if (numberIsTimesOf3(inputNumber) || inputNumberContainsGivenNumber(inputNumber, 3)) {
-            result = result + "Fizz";
+        if (inputNumberContainsGivenNumber(inputNumber, 3)) {
+            return "Fizz";
+        }
+        if (numberIsTimesOf3(inputNumber)) {
+            result += "Fizz";
         }
         if (numberIsTimesOf5(inputNumber)) {
-            result = result +"Buzz";
+            result += "Buzz";
         }
         if (numberIsTimesOf7(inputNumber)) {
-            result = result + "Whizz";
+            result += "Whizz";
         }
-        if (!numberIsTimesOf3(inputNumber) && !numberIsTimesOf5(inputNumber) && !numberIsTimesOf7(inputNumber)
-            && !inputNumberContainsGivenNumber(inputNumber, 3)) {
+        if (!numberIsTimesOf3(inputNumber) && !numberIsTimesOf5(inputNumber) && !numberIsTimesOf7(inputNumber)) {
             return String.valueOf(inputNumber);
         }
         return result;
