8a7310b3d326665e60bd596f8daed07e12d90fd5
qyzhao <qyzhao@thoughtworks.com>
2020-01-13 10:21:30 +0800

    [Qiaoying] add first test and implement: when given number is 6(times of 3) should return fizz.

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -3,63 +3,8 @@ package com.thoughtworks;
 class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
-    public static final String BUZZ = "Buzz";
-    public static final String WHIZZ = "Whizz";
 
-    public String convertNumber(int inputNumber) {
-        String result = "";
-        if (inputNumberContainsGivenNumber(inputNumber, 7)) {
-            if (numberIsTimesOf3(inputNumber)) {
-                result += FIZZ;
-            }
-            if (numberIsTimesOf7(inputNumber)) {
-                result += WHIZZ;
-            }
-        } else if (inputNumberContainsGivenNumber(inputNumber, 5)) {
-            result = getOutputForNumberIsTimesOf5And7(inputNumber, result);
-            return result;
-        } else {
-            if (inputNumberContainsGivenNumber(inputNumber, 3)) {
-                return FIZZ;
-            }
-            if (numberIsTimesOf3(inputNumber)) {
-                result += FIZZ;
-            }
-            result = getOutputForNumberIsTimesOf5And7(inputNumber, result);
-            if (!numberIsTimesOf3(inputNumber) && !numberIsTimesOf5(inputNumber) && !numberIsTimesOf7(inputNumber)) {
-                return String.valueOf(inputNumber);
-            }
-        }
-        return result;
-    }
-
-    private String getOutputForNumberIsTimesOf5And7(int inputNumber, String result) {
-        if (numberIsTimesOf5(inputNumber)) {
-            result += BUZZ;
-        }
-        if (numberIsTimesOf7(inputNumber)) {
-            result += WHIZZ;
-        }
-        return result;
-    }
-
-    private boolean numberIsTimesOf7(int inputNumber) {
-        return inputNumberIsTimesOfGivenNumber(inputNumber, 7);
-    }
-
-    private boolean numberIsTimesOf5(int inputNumber) {
-        return inputNumberIsTimesOfGivenNumber(inputNumber, 5);
-    }
-
-    private boolean numberIsTimesOf3(int inputNumber) {
-        return inputNumberIsTimesOfGivenNumber(inputNumber, 3);
-    }
-
-    private boolean inputNumberIsTimesOfGivenNumber(int inputNumber, int givenNumber) {
-        return inputNumber % givenNumber == 0;
-    }
-
-    private boolean inputNumberContainsGivenNumber(int inputNumber, int givenNumber) {
-        return String.valueOf(inputNumber).contains(String.valueOf(givenNumber));
+    public String fizzBuzz(int inputNumber) {
+        return FIZZ;
     }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -9,71 +9,7 @@ public class FizzBuzzTest {
     public void should_return_fizz_when_given_number_is_times_of_3_like_6() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         int inputNumber = 6;
-        String output = fizzBuzz.convertNumber(inputNumber);
-        assertEquals("Fizz", output);
-    }
-
-    @Test
-    public void should_return_buzz_when_given_number_is_times_of_5_like_10() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        int inputNumber = 10;
-        String output = fizzBuzz.convertNumber(inputNumber);
-        assertEquals("Buzz", output);
-    }
-
-    @Test
-    public void should_return_whizz_when_given_number_is_times_of_7_like_14() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        int inputNumber = 14;
-        String output = fizzBuzz.convertNumber(inputNumber);
-        assertEquals("Whizz", output);
-    }
-
-    @Test
-    public void should_return_fizzbuzz_when_given_number_is_times_of_3_and_5_like_15() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        int inputNumber = 60;
-        String output = fizzBuzz.convertNumber(inputNumber);
-        assertEquals("FizzBuzz", output);
-    }
-
-    @Test
-    public void should_return_number_when_given_number_is_not_times_of_3_or_5_or_7_like_4() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        int inputNumber = 4;
-        String output = fizzBuzz.convertNumber(inputNumber);
-        assertEquals("4", output);
-    }
-
-    @Test
-    public void should_return_fizz_when_given_number_is_contains_3_like_13() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        int inputNumber = 23;
-        String output = fizzBuzz.convertNumber(inputNumber);
-        assertEquals("Fizz", output);
-    }
-
-    @Test
-    public void should_return_fizz_when_given_number_is_contains_3_and_times_of_5_or_7_like_30() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        int inputNumber = 30;
-        String output = fizzBuzz.convertNumber(inputNumber);
-        assertEquals("Fizz", output);
-    }
-
-    @Test
-    public void should_return_buzzwhizz_when_given_number_is_contains_5_and_times_of_or_5_or_7_and_ignore_times_of_3_or_contains_3_like_35() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        int inputNumber = 35;
-        String output = fizzBuzz.convertNumber(inputNumber);
-        assertEquals("BuzzWhizz", output);
-    }
-
-    @Test
-    public void should_return_fizz_when_given_number_is_contains_7_and_times_of_3_and_ignore_times_of_5_or_contains_5_like_75() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        int inputNumber = 75;
-        String output = fizzBuzz.convertNumber(inputNumber);
+        String output = fizzBuzz.fizzBuzz(inputNumber);
         assertEquals("Fizz", output);
     }
 }
